0c92a94274c7ce70ea752e9cc723abef07da2c38
Christian Montesor <christianmontesor122@gmail.com>
2023-12-04 13:38:52 +0800

    norification progress

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,19 +1,10 @@
-// notification.component.ts
-
-import { Component, Input, OnInit } from '@angular/core';
-import { Post } from '../post.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.css']
 })
-export class NotificationComponent implements OnInit {
-  @Input() notification: { type: string, userId: string, timestamp: Date } = { type: '', userId: '', timestamp: new Date() };
-  @Input() post: Post = new Post('', '', '', '', '', '', new Date(), 0, [], [], '');
+export class NotificationComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 }
